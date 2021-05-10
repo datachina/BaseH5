586f39dd949e68130a14e7a5d5a37f781916db93
jidaojiuyou <2320541805@qq.com>
2021-05-10 09:55:16 +0800

    修复拼写错误

--- app/src/main/java/com/data/baseh5/config/Constant.java
+++ app/src/main/java/com/data/baseh5/config/Constant.java
@@ -5,6 +5,6 @@ package com.data.baseh5.config;
  * @date 2021-04-25
  * @since 2021-04-25
  */
-public class Constast {
+public class Constant {
     public static final String WEBVIEW_URL = "https://esalotto.cn/cq-parking-h5/";
 }
