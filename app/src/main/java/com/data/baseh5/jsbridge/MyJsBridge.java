de5561fb9259480b2df9e0e64618eb4d9a17d307
jidaojiuyou <2320541805@qq.com>
2021-08-20 16:12:56 +0800

    添加三个常用方法

--- app/src/main/java/com/data/baseh5/jsbridge/MyJsBridge.java
+++ app/src/main/java/com/data/baseh5/jsbridge/MyJsBridge.java
@@ -1,14 +1,21 @@
 package com.data.baseh5.jsbridge;
 
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.webkit.JavascriptInterface;
 
+import com.data.baseh5.util.ActivityController;
 import com.tencent.smtt.sdk.WebView;
 
 /**
+ * js桥
+ *
  * @author jidaojiuyou
- * @date 2021-04-23
+ * @date 2021-08-20
  * @since 2021-04-23
  */
+@SuppressWarnings("unused")
 public class MyJsBridge {
     /**
      * Context
@@ -22,5 +29,38 @@ public class MyJsBridge {
     public MyJsBridge(Context context, WebView webView) {
         this.context = context;
         this.webView = webView;
+    }
+
+
+    /**
+     * 打开浏览器
+     *
+     * @param url 链接
+     */
+    @JavascriptInterface
+    public void openBrowser(String url) {
+        Uri uri = Uri.parse(url);
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        context.startActivity(intent);
+    }
+
+    /**
+     * 打开拨号界面
+     *
+     * @param tel 电话号
+     */
+    @JavascriptInterface
+    public void openPhone(String tel) {
+        Uri uri = Uri.parse("tel:" + tel);
+        Intent intent = new Intent(Intent.ACTION_CALL, uri);
+        context.startActivity(intent);
+    }
+
+    /**
+     * 退出App
+     */
+    @JavascriptInterface
+    public void quit() {
+        ActivityController.finishAll();
     }
 }
