209425f0d2dbdd7a1efdc24d386653f91c53e22b
jidaojiuyou <2320541805@qq.com>
2021-08-20 16:12:21 +0800

    更换链接

--- app/src/main/java/com/data/baseh5/config/Constant.java
+++ app/src/main/java/com/data/baseh5/config/Constant.java
@@ -1,15 +1,19 @@
 package com.data.baseh5.config;
 
 /**
+ * 常量存储文件<br/>
+ * 常量多了可以使用分类方式创立内部类<br/>
+ * 可以参考{@code R}文件
+ *
  * @author jidaojiuyou
- * @date 2021-04-25
+ * @date 2021-08-20
  * @since 2021-04-25
  */
 public class Constant {
     /**
      * WebView的链接
      */
-    public static final String WEBVIEW_URL = "https://www.picb.cc/";
+    public static final String WEBVIEW_URL = "https://kotlinlang.org/";
     /**
      * 文件选择的code
      */
